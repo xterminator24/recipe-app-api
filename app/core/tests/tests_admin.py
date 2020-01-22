bc26c35faf86a4c6d9048dbcc67a0737e99e3e6b
Scott McKillop <scott.mckillop@outlook.com>
2020-01-22 11:28:02 -0400

    Configured Postgres DB

--- app/core/tests/tests_admin.py
+++ app/core/tests/tests_admin.py
@@ -37,5 +37,5 @@ class AdminSiteTests(TestCase):
         # Test that the create user page works
         url = reverse('admin:core_user_add')
         res = self.client.get(url)
-        
+
         self.assertEqual(res.status_code, 200)
